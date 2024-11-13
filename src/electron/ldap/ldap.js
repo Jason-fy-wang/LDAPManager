df74c20552936dd1e0f5fd57c5d1f40af52188be
jason <544094478@qq.com>
2024-11-13 23:13:39 +0800

    search dn and objectClass

--- src/electron/ldap/ldap.js
+++ src/electron/ldap/ldap.js
@@ -7,10 +7,11 @@ const client = new Client({
 
 async function LdapSearchAll() {
     try {
-        await client.bind("cn=admin,dc=example,dc=com",process.env.pwd)
+        await client.bind("cn=admin,dc=example,dc=com",process.env.passwd)
         console.log("connect success")
     }catch(ex) {
-        console.log("connect fail")
+        console.log("connect fail.")
+        throw ex
     }
 
     try {
@@ -31,28 +32,61 @@ async function LdapSearchAll() {
     }catch(ex) {
         console.log("search failed. ",ex)
     }finally {
-        client.unbind()
+        await client.unbind()
     }
 }
 
 async function LdapSearchWithDn(dn) {
     try {
-        await client.bind("cn=admin,dc=example,dc=com",process.env.pwd)
+        await client.bind("cn=admin,dc=example,dc=com",process.env.passwd)
         console.log("connect success")
     }catch(ex) {
         console.log("connect fail.")
+        throw ex
     }
 
     try {
         const {searchEntries, searchReferences} = await client.search(dn, {
             scope: "base"
         })
-
+        
+        searchEntries.forEach((entry) => {
+            console.log(entry)
+        })
         return searchEntries
     }catch(ex) {
         console.log("search error: ",ex)
+    }finally{
+        await client.unbind()
     }
 }
+
+async function LdapSearchObjectclass() {
+    try {
+        await client.bind("cn=admin,dc=example,dc=com",process.env.passwd)
+    }catch (ex) {
+        console.log("connect fail.")
+        throw ex
+    }
+
+    try {
+        const {searchEntries, searchReferences} = await client.search("cn=subschema", {
+            filter: "objectClass=subschema",
+            scope: "base",
+            attributes: ["objectClasses"]
+        })
+
+        searchEntries.forEach(element => {
+            console.log(element)
+        });
+    }catch(ex) {
+        throw ex
+    }finally {
+        await client.unbind()
+    }
+}
+
+LdapSearchObjectclass()
 
 
 module.exports = {LdapSearchAll, LdapSearchWithDn}
