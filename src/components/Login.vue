f0a76664ed39f5d470d222300ffde542d7cf3e08
jason <544094478@qq.com>
2025-06-02 23:22:22 +0800

    login unit test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,19 +3,19 @@
         <el-card style="max-width:50%;width:45vw;margin:100px auto;">
             <el-form @submit.prevent="handleLogin" @keyup.enter="handleLogin" class="login-form" label-width="80px">
                 <el-form-item label="Username">
-                    <el-input v-model="username" autocomplete="off" />
+                    <el-input v-model="username" autocomplete="off"  data-testid="username-input"/>
                 </el-form-item>
                 <el-form-item label="Password">
-                    <el-input v-model="password" type="password" autocomplete="off" />
+                    <el-input v-model="password" type="password" autocomplete="off" data-testid="pwd-input"/>
                 </el-form-item>
                 <el-form-item label="Host">
-                    <el-input v-model="host" autocomplete="off" />
+                    <el-input v-model="host" autocomplete="off" data-testid="host-input"/>
                 </el-form-item>
                 <el-form-item label="Port">
-                    <el-input v-model="port" autocomplete="off" />
+                    <el-input v-model="port" autocomplete="off" data-testid="port-input"/>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="handleLogin">Login</el-button>
+                    <el-button type="primary" @click="handleLogin" data-testid="login-btn">Login</el-button>
                     <el-button type="primary" @click="handleReset">Reset</el-button>
                 </el-form-item>
             </el-form>
--- test/components/Login.spec.js
+++ test/components/Login.spec.js
@@ -1,10 +1,12 @@
-import {mount, shallowMount} from '@vue/test-utils'
+import {flushPromises, mount, shallowMount} from '@vue/test-utils'
 import {expect, describe, it, beforeEach,vi} from 'vitest'
-import Login from '@/components/Login.vue'
-import useLdap from '@/pages/api/useLdap'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import {createTestingPinia} from '@pinia/testing'
 import { useObjectAttributes } from '@/store/ldapobjects'
 import { createRouter,createWebHistory } from 'vue-router'
+import {fireEvent, render} from '@testing-library/vue'
+import Login from '@/components/Login.vue'
 
 
 // Create mock router
@@ -33,9 +35,6 @@ vi.mock('@/pages/api/useLdap', () => ({
 }))
 
 describe('Login.vue',  () => {
-    beforeEach(async () => {
-        //await router.isReady()
-    })
     it('render properly', async()=> {
         const pinia = createTestingPinia({
             initialState: {
@@ -52,14 +51,45 @@ describe('Login.vue',  () => {
         const wrapper = mount(Login, {
             global: {
                 plugins: [
+                    ElementPlus,
                     pinia,
                     router
                 ]
             }
         })
-        const store = useObjectAttributes()
-        console.log("html:", wrapper.html())
-        expect(wrapper.find('el-button').text()).toBe('Login')
+        //console.log("html:", wrapper.html())  
+
+        //await wrapper.vm.$nextTick()
+        await flushPromises()
+        //console.log("element plus:", ElementPlus)
+
+        const userInput = wrapper.find('[data-testid="username-input"]')
+        const pwdInput = wrapper.find('[data-testid="pwd-input"]')
+        const hostInput = wrapper.find('[data-testid="host-input"]')
+        const port = wrapper.find('[data-testid="port-input"]')
+        expect(userInput.exists()).toBe(true)
+        expect(pwdInput.exists()).toBe(true)
+        expect(hostInput.exists()).toBe(true)
+        expect(port.exists()).toBe(true)
+
+        // input value
+        userInput.setValue('testuser')
+        pwdInput.setValue('testpassword')
+        hostInput.setValue('testhost')
+        port.setValue('389')
+        await flushPromises()
+
+        // begin trigger login function
+        const loginButton = wrapper.find('[data-testid="login-btn"]')
+        expect(loginButton.exists()).toBe(true)
+        await loginButton.trigger('click')
+        expect(loginMock).toHaveBeenCalledTimes(1)
+        expect(loginMock).toHaveBeenCalledWith(
+            'testhost',
+            '389',
+            'testuser',
+            'testpassword'
+        )
     })
 })
 
